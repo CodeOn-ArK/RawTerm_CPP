8c6df3c803fa742193d5a412ae3269ec1cfd52db
CodeOn-ArK <aarya.codeeka@gmail.com>
2021-04-03 18:40:36 +0530

    welcome window \b bug fixed

--- welcome2.cpp
+++ welcome2.cpp
@@ -1,73 +1,71 @@
 #include <ncurses.h>
+
 #include <iostream>
 #include <string>
 using namespace std;
 
-void trans_rec_win(){
+void trans_rec_win() {
+  initscr();
+  start_color();
+  cbreak();
+  noecho();
+  keypad(stdscr, TRUE);
 
-	initscr();
-	start_color();
-	cbreak();
-	noecho();
-	keypad(stdscr, TRUE);
-	
-	init_pair(1,COLOR_YELLOW,COLOR_BLACK);
-	for(int i = 2;i <= 45;i++)
-	  mvprintw(i,95,"|");
-	attron(COLOR_PAIR(1));
-	attron(A_BOLD);
-	attron(A_STANDOUT);
-	mvprintw(1,45,"TX");
-	mvprintw(1,140,"RX");
-	attroff(A_STANDOUT);
-	attroff(COLOR_PAIR(1));
-	mvprintw(47,5,"Enter here -->");
-	attroff(A_BOLD);
-	
-	refresh();
-	int ch ;
-	string line;
-	/*  while (1) {
-	    ch = cin.gets();
-	    cout.put(ch);
+  init_pair(1, COLOR_YELLOW, COLOR_BLACK);
+  for (int i = 2; i <= 45; i++) mvprintw(i, 95, "|");
+  attron(COLOR_PAIR(1));
+  attron(A_BOLD);
+  attron(A_STANDOUT);
+  mvprintw(1, 45, "TX");
+  mvprintw(1, 140, "RX");
+  attroff(A_STANDOUT);
+  attroff(COLOR_PAIR(1));
+  mvprintw(47, 5, "Enter here -->");
+  attroff(A_BOLD);
 
-	    if (ch == '\r') break;
-            
-	  }*/
-	for(int j = 2;j <= 180;j++)
-		mvprintw(45,j,"_");
-	int i = 4, col = 0;
-	move(47,20);
-	while (ch = wgetch(stdscr)) {
-		if (ch == '\n'){
-			mvprintw(i,2,line.c_str());
-			move(47,19);
-			clrtoeol();
-			i++;
-			line.clear();
-			col = 0;
-			move(47, 20);
-		} else if (ch == KEY_BACKSPACE) {
-			line.pop_back();
-			move(47, 20); clrtoeol();
-			mvprintw(47,20,line.c_str());
-			move(47, 20 + --col);
-		} else {
-			line.push_back(ch);
-			mvprintw(47,20,line.c_str());
-			move(47,20 + ++col);
-		}
+  refresh();
+  int ch;
+  string line;
+  /*  while (1) {
+      ch = cin.gets();
+      cout.put(ch);
 
-		if(line == "quit") break;
-	}
+      if (ch == '\r') break;
 
-	getch();
-	endwin();
+    }*/
+  for (int j = 2; j <= 180; j++) mvprintw(45, j, "_");
+  int i = 4, col = 0;
+  move(47, 20);
+  while (ch = wgetch(stdscr)) {
+    if (ch == '\n') {
+      mvprintw(i, 2, line.c_str());
+      move(47, 19);
+      clrtoeol();
+      i++;
+      line.clear();
+      col = 0;
+      move(47, 20);
+    } else if (ch == KEY_BACKSPACE) {
+      line.pop_back();
+      move(47, 20);
+      clrtoeol();
+      mvprintw(47, 20, line.c_str());
+      move(47, 20 + --col);
+    } else {
+      line.push_back(ch);
+      mvprintw(47, 20, line.c_str());
+      move(47, 20 + ++col);
+    }
+
+    if (line == "quit") break;
+  }
+
+  getch();
+  endwin();
 }
 
-int main(){
-	
-	trans_rec_win();
-	
-	return 0;
-}	
+int main() {
+  trans_rec_win();
+
+  return 0;
+}
