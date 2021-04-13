a81f8ded1b46a7c529dbe41de718c32c8635f197
CodeOn-ArK <aarya.codeeka@gmail.com>
2021-04-13 17:30:59 +0530

    logo added in welcome screen

--- welcome.cpp
+++ welcome.cpp
@@ -2,17 +2,44 @@
 void wlkm() {
   initscr();
   start_color();
-  mvprintw(30, 80, "      ______");
-  mvprintw(31, 80, "  ___/_  __/");
-  mvprintw(32, 80, " / __// /");
-  mvprintw(34, 80, "/_/  /_/");
-  mvprintw(30, 70, "   ___             ______");
-  mvprintw(31, 70, "  / _ \___ __    _/_  __/__ ______ _  ");
-  mvprintw(31, 70, " / , _/ _ `/ |/|/ // / / -_) __/  ' \ ");
-  mvprintw(31, 70, "/_/|_|\_,_/|__,__//_/  \__/_/ /_/_/_/");
   init_pair(1, COLOR_GREEN, COLOR_BLACK);
   init_pair(2, COLOR_YELLOW, COLOR_BLACK);
   init_pair(3, COLOR_RED, COLOR_BLACK);
+  init_pair(4, COLOR_CYAN, COLOR_BLACK);
+  assume_default_colors(COLOR_YELLOW, COLOR_BLACK);
+  attron(A_BLINK);
+  mvprintw(10, 85, "      ______");
+  mvprintw(11, 85, "  ___/_  __/");
+  attron(COLOR_PAIR(1));
+  mvprintw(12, 85, " / __// /");
+  mvprintw(13, 85, "/_/  /_/");
+  refresh();
+  for (uint32_t i = 0; i < 0xfffffff; i++)
+    ;
+  for (int i = 10; i <= 13; i++) {
+    move(i, 0);
+    clrtoeol();
+  }
+  attroff(A_BLINK);
+  attron(COLOR_PAIR(2));
+  mvprintw(10, 75, "   ___             ______");
+  attron(COLOR_PAIR(1));
+  mvprintw(11, 75, "  / _ \\___ __    _/_  __/__ ______ _  ");
+  attron(COLOR_PAIR(3));
+  mvprintw(12, 75, " / , _/ _ `/ |/|/ // / / -_) __/  ' \\ ");
+  attron(COLOR_PAIR(4));
+  mvprintw(13, 75, "/_/|_|\\_,_/|__,__//_/  \\__/_/ /_/_/_/");
+  refresh();
+  for (uint32_t i = 0; i < 0xffffffff / 20; i++)
+    ;
+  for (int i = 10; i <= 13; i++) {
+    move(i, 0);
+    clrtoeol();
+  }
+  assume_default_colors(COLOR_WHITE, COLOR_BLACK);
+  attroff(COLOR_PAIR(1));
+  attroff(A_BLINK);
+  refresh();
   attron(A_BOLD);
   attron(COLOR_PAIR(3));
   mvprintw(15, 5,
@@ -52,5 +79,6 @@ void wlkm() {
     clrtoeol();
   }
 
+  (void)ch;
   endwin();
 }
